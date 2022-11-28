50d61db5c7c49815bf42ca4daf5bf81365641314
kixzeit <josh.jgp99@gmail.com>
2022-11-28 10:49:22 -0600

    try

---
+++ main.js
@@ -0,0 +1,47 @@
+// function increasePoblation(actual, incremento,años) {
+
+//   for (let i = 1; i <= años; i++){
+//     actual+= actual*incremento
+//   }
+//   return actual
+// }
+
+// console.log(increasePoblation(1758, 0.03, 5));
+
+// function makeExercices(actual, incremento,dias) {
+//   for (let i = 1; i <= dias; i++) {
+//     actual += actual + incremento;
+//   }
+//   return actual;
+// }
+
+
+// console.log(makeExercices(1, 2, 19))
+
+// 12 14
+// 3 16
+
+// let p11 = 8 + 10 = 18;
+// let p12 = 12 + 20 =32
+// let p21 = 2 + 12=14
+// let p22 = 3 + 24 = 27
+
+// 3 + 10=13
+// 5 + 50=55
+// 21 + 2=23
+// 35+10=45
+
+// 3 4
+// 8 12
+// 4 5
+// 7 2
+
+// 3 6
+// 4 2
+// 7 0
+
+
+// 3 +2 5
+// 8 + 10 18 
+// 6 + 8 14
+// 16 + 40 56
